8251e3f98909747cf7b84c922d8da1a52a4488f8
SohumDamani <63761712+SohumDamani@users.noreply.github.com>
2020-11-22 21:45:51 +0530

    Update qr_detect.py

--- qr_detect.py
+++ qr_detect.py
@@ -22,47 +22,30 @@ class image_proc():
 	# Initialise everything
 	def __init__(self):
 
-		self.lat =Float32()
-		self.lon =Float32()
-		self.alt =Float32()
+		self.target=String()
+		self.target.data = ""
 
-		self.lat.data = 0
-		self.lon.data = 0
-		self.alt.data = 0
-
-		rospy.init_node('barcode_test') #Initialise rosnode 
-		self.set_latitude = rospy.Publisher("/target_latitude",Float32,queue_size=1)#publish the target point
-		self.set_longitude = rospy.Publisher("/target_longitude",Float32,queue_size=1)
-		self.set_altitude = rospy.Publisher("/target_altitude",Float32,queue_size=1)
-		
+		rospy.init_node('barcode_test') #Initialise rosnode 	
+		self.target_pub = rospy.Publisher("/target",String,queue_size=1)
 		self.image_sub = rospy.Subscriber("/edrone/camera/image_raw", Image, self.image_callback) #Subscribing to the camera topic
-		self.img = np.empty([]) # This will contain your image frame from camera
 		self.bridge = CvBridge()
 
 
 	# Callback function of amera topic
 	def image_callback(self, data):
 		try:
-			self.img = self.bridge.imgmsg_to_cv2(data, "bgr8") # Converting the image to OpenCV standard image
+			img = self.bridge.imgmsg_to_cv2(data, "bgr8") # Converting the image to OpenCV standard image
 		except CvBridgeError as e:
 			print(e)
 			return
-	def qrcode(self):
-		barcodes = decode(self.img)
+		barcodes=[]
+		barcodes = decode(img)
 		for barcode in barcodes:
 			barcodeData = barcode.data.decode("utf-8")
-			uts = barcodeData.encode('ascii', 'ignore')
-			loc = uts.split(',')
-			self.lat.data = float(loc[0])
-			self.lon.data = float(loc[1])
-			self.alt.data = float(loc[2])
+			self.target.data = barcodeData
 
-			self.set_latitude.publish(self.lat)
-			self.set_longitude.publish(self.lon)
-			self.set_altitude.publish(self.alt)
+			self.target_pub.publish(self.target)
 
 if __name__ == '__main__':
     image_proc_obj = image_proc()
     rospy.spin()
-    while True:
-    	image_proc_obj.qrcode()
